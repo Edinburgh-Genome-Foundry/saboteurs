569f1f384604d71b215632d432a91b5c5cd50671
Zulko <valentin.zulkower@gmail.com>
2018-12-12 13:35:11 +0000

    v0.2.0

--- setup.py
+++ setup.py
@@ -9,9 +9,11 @@ setup(
     name='saboteurs',
     version=__version__,
     author='Zulko',
+
     description='Identify agents impairing success accross experiments.',
     long_description=open('pypi-readme.rst').read(),
     license='see LICENSE.txt',
+    url='https://github.com/Edinburgh-Genome-Foundry/saboteurs',
     keywords="statistics weakest link DNA part validation",
     packages=find_packages(exclude='docs'),
     include_package_data=True,
