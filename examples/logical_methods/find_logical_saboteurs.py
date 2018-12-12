2160629cc94699bfcfb9c89df95ceffd4a1d8445
Zulko <valentin.zulkower@gmail.com>
2018-12-12 16:52:46 +0000

    changed example

--- examples/logical_methods/find_logical_saboteurs.py
+++ examples/logical_methods/find_logical_saboteurs.py
@@ -1,10 +1,15 @@
 from saboteurs import find_logical_saboteurs
 
+# NOTE: the teo definitions below could also be read from a CSV:
+# groups, failed_groups = csv_to_groups_data('find_logical_saboteurs.csv')
+
 groups = {
     1: ['A', 'C', 'D'],
     2: ['B', 'C', 'E'],
     3: ['A', 'B', 'D'],
     4: ['D', 'F', 'G']
 }
-result = find_logical_saboteurs(groups, failed_groups=[2, 4])
+failed_groups = [2, 4]
+
+result = find_logical_saboteurs(groups, failed_groups=failed_groups)
 print (result)
